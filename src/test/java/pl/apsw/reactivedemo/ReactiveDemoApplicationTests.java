ee902fce5445cd067651c7fb6f54d5d79f75b95f
Piotr Solarz-Wnek <piotr.solarz-wnek@exalo.pl>
2021-06-20 22:22:56 +0200

    initial commit for reactive demo

---
+++ src/test/java/pl/apsw/reactivedemo/ReactiveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.apsw.reactivedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
